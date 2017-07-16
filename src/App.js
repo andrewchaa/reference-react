5e49277a83c71d49bf4bec9ad9245d8f18af2f93
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2017-07-16 10:00:39 +0100

    game

--- src/App.js
+++ src/App.js
@@ -2,21 +2,60 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Game extends Component {
-  render() {
-    return (
+const Stars = (props) => {
+  return (
+    <div className="col-5">
+      <i className="fa fa-star"></i>
+      <i className="fa fa-star"></i>
+      <i className="fa fa-star"></i>
+      <i className="fa fa-star"></i>
+    </div>
+
+  )
+}
+
+const Button = (props) => {
+  return (
+    <div className="col-2">
+      <button>=</button>
+    </div>
+  )
+}
+
+const Answer = (props) => {
+  return (
+    <div className="col-5">
+      ...
+    </div>
+  )
+}
+
+const Numbers = (props) => {
+  return (
+    <div className="card text-center">
       <div>
-        <h3>Play Nine</h3>
+        <span>1</span>
+        <span className="selected">2</span>
+        <span className="used">3</span>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Game />        
+      <div className="container">
+        <h3>Play Nine</h3>
+        <hr />
+
+        <div className="row">
+          <Stars />
+          <Button />
+          <Answer />        
+        </div>
+        <br />
+        <Numbers />
       </div>
     );
   }
